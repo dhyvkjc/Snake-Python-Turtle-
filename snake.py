dc758ffcb5d6bf78e490b2773030336b6ad3a885
dhyvkjc <xddd246820@gmail.com>
2026-05-08 16:32:47 -0500

    Update snake.py

--- snake.py
+++ snake.py
@@ -8,7 +8,7 @@ def playing_area():
     pen = Turtle()
     pen.ht()
     pen.speed(0)
-    pen.color('light blue')
+    pen.color('black')
     pen.begin_fill()
     pen.goto(-240,240)
     pen.goto(240,240)
@@ -20,54 +20,121 @@ def playing_area():
 class Head(Turtle):
   def __init__(self, screen, body):
     super().__init__()
-    pass
+    self.alive = True
+    self.speed(0)
+    self.shape("square")
+    self.color("black")
+    self.penup()
+    self.goto(0,0)
+
+    self.direction = ("up", "down", "left", "right")
+    screen.onkeypress(self.up, "Up")
+    screen.onkeypress(self.down, "Down")
+    screen.onkeypress(self.left, "Left")
+    screen.onkeypress(self.right, "Right")
+
 
   def up(self):
-    pass
+    if self.direction != "down":
+      self.direction = "up"
+      self.setheading(90)
+
 
   def down(self):
-    pass
+    if self.direction != "up":
+      self.direction = "down"
+      self.setheading(-90)
 
   def left(self):
-    pass
+    if self.direction != "right":
+      self.direction = "left"
+      self.setheading(180)
 
   def right(self):
-    pass
+    if self.direction != "left":
+      self.direction = "right"
+      self.setheading(0)
 
   def move(self):
-    pass
-    
+    self.forward(20)
+    if self.xcor() > 240 or self.xcor()< -240 or self.ycor() > 240 or self.ycor() < -240:
+      self.alive = False 
+
+
   def die(self):
-    pass
-
-
+    if self.alive == False:
+      head.hideturtle()
+      
 class Segment(Turtle):
   def __init__(self, other):
     super().__init__()
-    pass
+    self.ht()
+    self.speed(0)
+    self.shape("square")
+    self.color("green")
+    self.penup()
+    self.goto(other.xcor(),other.ycor())
+    self.st()
+    
 
   def move(self, other):
-    pass
+    self.goto(other.xcor(),other.ycor())
 
 class Apple(Turtle):
   def __init__(self):
     super().__init__()
-    pass
+    self.speed(0)
+    self.shape("circle")
+    self.color("red")
+    self.penup()
+    self.goto(100,100)
 
   def relocate(self):
-    pass
+      x = random.randint(-230,230)
+      y = random.randint(-230,230)
+      self.goto(x,y)
+
+def update():
+  if p1.alive == True:
+    p1.move()
+
+    # moves the snake body
+    for i in range(len(body) -1, 0, -1):
+      body[i].move(body[i - 1])
+
+    # Collision detection
+    if p1.distance(apple) < 20:
+      apple.relocate()
+      body.append(Segment(body[-1]))
+
+
+   
+
+
+    
+
+
+  screen.ontimer(update, 120)
+
+
+#####################################################################################################################################################################################
 
 screen = Screen()
-screen.bgcolor("black")
+screen.bgcolor("lime green")
 screen.setup(520,520)
-# Key Binding. Connects key presses and mouse clicks with function calls
 screen.listen()
+screen.onkey(update, "space")
 
-body = []
 
 
-screen.exitonclick()
+body = [] 
 
+
+p1 = Head(screen, body)
+body.append(p1)
+body.append(Segment(body[-1]))
+body[1].color("black")
+apple = Apple()
 
 
 
